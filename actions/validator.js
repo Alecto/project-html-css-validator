f4e23621b98d77fe41f6eb85dc4c08809fd987d3
Alecto <fomenkoandrey1978@gmail.com>
2023-01-25 14:06:59 +0200

    обновление версии зависимостей, исправление ошибок + мелкие доработки

--- actions/validator.js
+++ actions/validator.js
@@ -1,10 +1,10 @@
-import glob from "glob";
-import cssValidator from "w3c-css-validator";
-import fs from "fs";
-import chalk from "chalk";
-import { w3cHtmlValidator } from "w3c-html-validator";
+import glob from 'glob';
+import cssValidator from 'w3c-css-validator';
+import fs from 'fs';
+import chalk from 'chalk';
+import { w3cHtmlValidator } from 'w3c-html-validator';
 
-import DATA from "./settings.json" assert { type: "json" };
+import DATA from './settings.json' assert { type: 'json' };
 
 const log = console.log;
 const HTML_FILES = DATA.htmlFiles;
@@ -15,24 +15,30 @@ const EXCLUDE_FILES = DATA.ignore;
 function htmlValidation() {
   let counter = 0;
 
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     glob(HTML_FILES, (err, files) => {
       let filesFiltered = files.filter(
         (file) => !EXCLUDE_FILES.some((str) => file.indexOf(str) !== -1)
       );
 
-      printTitle(filesFiltered.length, "HTML");
+      printTitle(filesFiltered.length, 'HTML');
 
       if (!filesFiltered.length) resolve(null);
 
       filesFiltered.forEach((file) => {
-        fs.readFile(file, "utf8", (err, data) => {
+        fs.readFile(file, 'utf8', (err, data) => {
           if (err) return;
 
-          w3cHtmlValidator.validate({ filename: file }).then((res) => {
-            w3cHtmlValidator.reporter(res);
-            if (filesFiltered.length === ++counter) resolve("finished");
-          });
+          w3cHtmlValidator
+            .validate({ filename: file })
+            .then((res) => {
+              w3cHtmlValidator.reporter(res, {
+                continueOnFail: true,
+                maxMessageLen: 200
+              });
+              if (filesFiltered.length === ++counter) resolve('ok'); // Не используется, но можно отследить результат
+            })
+            .catch(() => resolve('errors')); // Не используется, но можно отследить ошибку
         });
       });
     });
@@ -49,32 +55,32 @@ function cssValidation() {
         (file) => !EXCLUDE_FILES.some((str) => file.indexOf(str) !== -1)
       );
 
-      printTitle(filesFiltered.length, "CSS");
+      printTitle(filesFiltered.length, 'CSS');
 
       if (!filesFiltered.length) resolve(null);
 
       filesFiltered.forEach((file) => {
-        fs.readFile(file, "utf8", (err, data) => {
+        fs.readFile(file, 'utf8', (err, data) => {
           if (err) return;
 
           validateCSS(data).then((res) => {
-            console.log(" ----- Тестирование файла... ----- ");
+            console.log(' ----- Тестирование файла... ----- ');
 
             if (res.valid) {
               log(
                 ` ${chalk.green.bold(file)} ${chalk.black.bgGreen(
-                  " Валиден "
+                  ' Валиден '
                 )} `
               );
             } else {
               log(
-                ` ${chalk.red.bold(file)} ${chalk.white.bgRed(" НЕ валиден ")} `
+                ` ${chalk.red.bold(file)} ${chalk.white.bgRed(' НЕ валиден ')} `
               );
             }
 
             if (!res.valid) console.log(res.errors);
 
-            if (filesFiltered.length === ++counter) resolve("finished");
+            if (filesFiltered.length === ++counter) resolve('finished');
           });
         });
       });
@@ -97,17 +103,20 @@ async function validateCSS(data) {
   }
 
   const result = await cssValidator.validateText(data, {
-    medium: "all",
-    timeout: 3000,
+    medium: 'all',
+    timeout: 3000
   });
 
   return result;
 }
 
-console.log("\n");
-htmlValidation().then((res) => {
-  console.log("\n");
-  cssValidation().then(() => {
-    console.log("\n");
-  });
-});
+console.log('\n');
+
+htmlValidation()
+  .then((res) => {
+    console.log('\n');
+    cssValidation().then(() => {
+      console.log('\n');
+    });
+  })
+  .catch(() => { });
