06a609105740f12de186aba57c744a2e14c6970f
Alecto <fomenkoandrey1978@gmail.com>
2025-04-29 10:22:45 +0300

    Feat(constants): add global ignore directories and update ignore patterns

--- actions/constants.mjs
+++ actions/constants.mjs
@@ -9,9 +9,12 @@ export const DEFAULT_CSS_VALIDATOR_OPTIONS = {
 // Патерни пошуку файлів
 export const HTML_FILES_PATTERN = './**/*.html';
 export const CSS_FILES_PATTERN = './**/*.css';
+
+// Глобальні каталоги, що повністю ігноруються (не перевіряються ніколи)
+export const GLOBAL_IGNORE_DIRS = ['node_modules', 'vendors', 'libs'];
+
+// Патерни для ігнорування файлів
 export const IGNORE_PATTERNS = [
-  'node_modules',
-  'libs',
   'bootstrap',
   'bootstrap-5',
   'normalize.css',
